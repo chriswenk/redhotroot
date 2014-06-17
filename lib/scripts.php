070e96abeff8b1701c778731d73567b277ddb5b7
Chris <Personal@Chris-Wenks-MacBook-Air.local>
2014-06-17 21:14:14 +0200

    scroll top button

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c4ad6897d507493c945e5eda08ce6e65');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '266f5ce629bc168480bffe205a4f430f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
